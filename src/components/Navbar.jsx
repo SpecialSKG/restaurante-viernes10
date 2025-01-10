ec7ff816e5c82e7ade9118f22565debedc437399
SpecialSKG <josealvaro21@hotmail.com>
2025-01-10 11:56:45 -0600

    #2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,38 +4,53 @@ const Navbar = () => {
     return (
         <>
             <div className="navbar bg-base-100">
-                <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            className="inline-block h-5 w-5 stroke-current">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h16M4 18h16"></path>
-                        </svg>
-                    </button>
-                </div>
-                <div className="flex-1">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Inicio</a></li>
+                            <li>
+                                <a>Productos</a>
+                                <ul className="p-2">
+                                    <li><a>Nuevos Productos</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                     <a className="btn btn-ghost text-xl">Restaurante</a>
                 </div>
-                <div className="flex-none">
-                    <button className="btn btn-square btn-ghost">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            className="inline-block h-5 w-5 stroke-current">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-                        </svg>
-                    </button>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <details>
+                                <summary>Parent</summary>
+                                <ul className="p-2">
+                                    <li><a>Submenu 1</a></li>
+                                    <li><a>Submenu 2</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Catalogo</a>
                 </div>
             </div>
         </>
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -5,6 +5,53 @@ const Inicio = () => {
         <div className="min-h-full">
             <main>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+                    <div
+                        className="hero min-h-screen"
+                        style={{
+                            backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                        }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-neutral-content text-center">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">
+                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                </p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="flex w-full flex-col">
+                        <div className="divider">--------</div>
+                    </div>
+
+                    <div className="card bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img
+                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-outline">Fashion</div>
+                                <div className="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex w-full flex-col">
+                        <div className="divider">--------</div>
+                    </div>
+
                     <div className="carousel carousel-center rounded-box">
                         <div className="carousel-item">
                             <img src="https://www.estudiahosteleria.com/blog/wp-content/uploads/2019/05/distribucion-restaurante-esah.jpg" alt="Pizza" />
@@ -37,33 +84,14 @@ const Inicio = () => {
                         </div>
                     </div>
 
-
-                    <div className="flex w-full flex-col border-opacity-50">
-                        <div className="card bg-base-300 rounded-box grid h-20 place-items-center"></div>
-                    </div>
-
-                    <div className="card bg-base-100 w-96 shadow-xl">
-                        <figure>
-                            <img
-                                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title">
-                                Shoes!
-                                <div className="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <div className="badge badge-outline">Fashion</div>
-                                <div className="badge badge-outline">Products</div>
-                            </div>
-                        </div>
+                    <div className="join">
+                        <button className="join-item btn">1</button>
+                        <button className="join-item btn btn-active">2</button>
+                        <button className="join-item btn">3</button>
+                        <button className="join-item btn">4</button>
                     </div>
 
                 </div>
-
-
 
             </main>
         </div>
